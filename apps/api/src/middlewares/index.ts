34beb3c99364402efb1c1707e0d0563412223bdc
Laxman Rathod <laxmanrathod.dev@gmail.com>
2026-01-25 03:32:54 +0530

    refactor(api): add barrel export for middleware modules

---
+++ apps/api/src/middlewares/index.ts
@@ -0,0 +1,4 @@
+export { asyncHandler } from "./async-handler";
+export { isAuthenticated } from "./auth-check";
+export { errorHandler } from "./error-handler";
+export { rateLimiter } from "./rate-limit";
