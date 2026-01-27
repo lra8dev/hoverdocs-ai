25b4a4a95fca53ea16a0ce7513265b754efe0172
Laxman Rathod <laxmanrathod.dev@gmail.com>
2026-01-28 01:11:11 +0530

    feat(extension): add server URL, publisher, and identifier to config

---
+++ apps/extension/src/config/index.ts
@@ -0,0 +1,9 @@
+export const config = {
+  X_PUBLISHER: "lucky",
+  X_IDENTIFIER: "hoverdocs-ai",
+  // Development
+  // SERVER_API_URL: "http://localhost:5000",
+
+  // Production
+  SERVER_API_URL: "https://hoverdocs-ai-production.up.railway.app",
+};
